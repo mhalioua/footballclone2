9fd36a9d06247073202442dcd4b84849787d5865
higherstar <higherstar@outlook.com>
2017-12-29 16:30:08 +0800

    fix

--- db/migrate/20171229081831_add_lines_to_export.rb
+++ db/migrate/20171229081831_add_lines_to_export.rb
@@ -1,10 +1,10 @@
 class AddLinesToExport < ActiveRecord::Migration[5.1]
   def change
-  	add_column :stadia, :first_side, :float
-  	add_column :stadia, :second_side, :float
-  	add_column :stadia, :full_side, :float
-  	add_column :stadia, :first_total, :float
-  	add_column :stadia, :second_total, :float
-  	add_column :stadia, :full_total, :float
+  	add_column :exports, :first_side, :float
+  	add_column :exports, :second_side, :float
+  	add_column :exports, :full_side, :float
+  	add_column :exports, :first_total, :float
+  	add_column :exports, :second_total, :float
+  	add_column :exports, :full_total, :float
   end
 end
