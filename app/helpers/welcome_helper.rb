10b6ce327ae3c7de13dcda2e89e964d71bd23e84
Apollo <super.hero2017@outlook.com>
2018-09-12 14:04:41 +0200

    Fix

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,26 @@
 module WelcomeHelper
+  def lineToString(home, away, home_abbr, away_abbr)
+    pinnacle = ""
+    if home
+      home, away = away, home if home[0] != "-"
+
+      index = home.index(" ")
+      pinnacle = index ? home[0..index - 1] : ""
+
+      index = away.index(" ")
+      if index
+        pinnacle = pinnacle + " and " if pinnacle != ""
+        pinnacle = pinnacle + away[0..index - 1]
+      end
+
+    end
+    if pinnacle != ""
+      if away == "-"
+        pinnacle = home_abbr + " was " + pinnacle
+      else
+        pinnacle = away_abbr + " was " + pinnacle
+      end
+    end
+    return pinnacle
+  end
 end
