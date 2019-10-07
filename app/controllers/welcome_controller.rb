4ce5e5927c709753b5f01f9236b221e96b2027e1
Apollo <super.hero2017@outlook.com>
2019-10-07 04:07:57 -0400

    Tst

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,31 @@
 class WelcomeController < ApplicationController
-	
-	before_action :confirm_logged_in
 
-	def index
-			unless params[:teamPicker]
-				params[:teamPicker] = false
-			end
-			@teamPicker = params[:teamPicker]
-	  	unless params[:id]
-	  	  	params[:id] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
-	  	end
-	  	@game_index = params[:id]
-	  	@game_start_index = @game_index[0..9]
-	  	@game_end_index = @game_index[13..23]
-			if params[:teamPicker]
-				@games = []
-			else
-				@games = Game.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
-							.order("game_state")
-							.order("game_status")
-							.order("game_date")
-			end
-	end
+  before_action :confirm_logged_in
+
+  def index
+    unless params[:teamPicker]
+      params[:teamPicker] = false
+    end
+    @teamPicker = params[:teamPicker]
+    unless params[:id]
+      params[:id] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
+    end
+    @game_index = params[:id]
+    if params[:teamPicker]
+			@games = Game.where("home_team = ?", @game_index).or(Game.where("away_team = ?", @game_index))
+									 .order("game_state")
+									 .order("game_status")
+									 .order("game_date")
+    else
+			@game_start_index = @game_index[0..9]
+			@game_end_index = @game_index[13..23]
+      @games = Game.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
+                   .order("game_state")
+                   .order("game_status")
+                   .order("game_date")
+    end
+    @teams = Game.distinct.pluck(:home_team)
+		@away_teams = Game.distinct.pluck(:away_team)
+		@teams.concat(@away_teams).uniq
+  end
 end
