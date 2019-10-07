19dbf8beeef65992ee07981c9f78521deadeb7f9
Apollo <super.hero2017@outlook.com>
2019-10-07 04:51:10 -0400

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,12 +11,12 @@ class WelcomeController < ApplicationController
       params[:id] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
     end
     @game_index = params[:id]
+    @game_start_index = @game_index[0..9]
+    @game_end_index = @game_index[13..23]
     if @teamPicker != false
 			@games = Game.where("home_team = ? AND game_date < ?", @teamPicker, Date.today).or(Game.where("away_team = ? AND game_date < ?", @teamPicker, Date.today))
 									 .order("game_date")
     else
-			@game_start_index = @game_index[0..9]
-			@game_end_index = @game_index[13..23]
       @games = Game.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
                    .order("game_state")
                    .order("game_date")
