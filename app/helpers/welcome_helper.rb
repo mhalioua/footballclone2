c1625ccd1560b9546e809ad10446beef8bc9462a
Apollo <super.hero2017@outlook.com>
2019-10-21 04:19:56 -0400

    Fix issue

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -15,7 +15,7 @@ module WelcomeHelper
   end
 
   def lineToString(home, away)
-    return if home == nil || away == nil || home == '' || away == ''
+    return '', '' if home == nil || away == nil || home == '' || away == ''
     if home[0] == "-"
       spread = stringTofloat(home)
       total = stringTofloat(away)
