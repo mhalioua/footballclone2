f021f972ed1d79d8ef42c7a41ba876cd1be01cb4
hammad417 <hammad417@gmail.com>
2020-01-24 18:10:43 +0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 	
   root 'welcome#index'
 
-  match ':controller(/:action(/:id))', :via => [:get, :post]
+  match ':controller(/:action(/:id))', :via => [:get, :post] 
 end
